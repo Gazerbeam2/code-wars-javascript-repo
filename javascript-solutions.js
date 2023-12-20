d64a3aaeb48621fc55803514431f235ba3ebda6b
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2023-12-19 22:09:39 -0600

    added eigth solution

--- javascript-solutions.js
+++ javascript-solutions.js
@@ -79,3 +79,14 @@ function reverseList(list){
 }
 
 ___________________________________________________________________________________________________
+
+//You Can't Code Under Pressure #1
+//Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i){
+  // i will be and integer. Double it and return it.
+
+  return i * 2;
+}
+
+_______________________________________________________________________________________________________________________
