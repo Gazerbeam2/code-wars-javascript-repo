834249175b8a25eb83fe52732902bf190375dbd6
Gazerbeam2 <134249845+Gazerbeam2@users.noreply.github.com>
2023-12-24 01:43:07 -0600

    feat: added twelfth solution

--- javascript-solutions.js
+++ javascript-solutions.js
@@ -147,3 +147,25 @@ function oddOrEven(array) {
    }
 
    ___________________________________________________
+
+  //Find the middle element
+  //As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+  //The input to the function will be an array of three distinct numbers
+
+  function gimme (triplet){
+    let minValue = Math.min(...triplet);
+    let maxValue = Math.max(...triplet);
+
+    for(let index = 0; index < triplet.length; index++){
+      if(triple[index] > minValue && triplet[index] < maxValue){
+        return index;
+      }
+    }
+  }
+
+  /*function gimme(a) {
+  return a.indexOf(a.concat().sort(function(a, b) { return a - b })[1])
+}*/
+
+_____________________________________________________________________________________
