7ecdb0c305dee2a32344b5d9dbe4dea9b2b06e54
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2023-12-25 18:02:49 -0600

    added thirteenth solution

--- javascript-solutions.js
+++ javascript-solutions.js
@@ -169,3 +169,16 @@ function oddOrEven(array) {
 }*/
 
 _____________________________________________________________________________________
+
+//Round up to the next multiple of 5
+//Give an integer as input, can you round it to the next(meaing, "greater than or equal") multiple of 5?
+
+function roundToNext5(n){
+  return Math.ceil(n / 5) * 5;
+}
+
+/*function roundToNext5(n){
+  while(n % 5 !== 0) n++;
+  return n;
+}*/
+____________________________________________________________________________________________________________________________________
