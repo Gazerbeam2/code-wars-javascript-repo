40dad1f07129e0f2e08d031b547dadf5d7695ae3
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2023-12-26 18:05:29 -0600

    added fourteenth solution

--- javascript-solutions.js
+++ javascript-solutions.js
@@ -182,3 +182,12 @@ function roundToNext5(n){
   return n;
 }*/
 ____________________________________________________________________________________________________________________________________
+
+//Beginnner Series #4 Cockroach
+/* The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (=floored) */
+
+function cockroachSpeed(s){
+    return Math.floor(s * 27.778)
+}
+
+_________________________________________________________________________________________________________________________________________
