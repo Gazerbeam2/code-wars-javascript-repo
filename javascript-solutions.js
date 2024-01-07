057740d76361f4a1f431f567a8a4c6580fe5a771
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2024-01-06 18:55:36 -0600

    added sixteenth solution

--- javascript-solutions.js
+++ javascript-solutions.js
@@ -216,3 +216,11 @@ function countSheeps(sheep) {
  }
 
  ________________________________________________________________________________________________________________________________________
+
+ //Complete the solution so that the function will break up camel casing, using a space between words.
+
+ function solution(string){
+  return (string.replace(/([A-Z])/g, '  $1'));
+ }
+
+ ____________________________________________________________________________________________________
