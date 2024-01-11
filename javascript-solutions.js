2feebfbeec76dfb5074a5937f97dab3df395c0c2
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2024-01-10 19:58:17 -0600

    added nineteeth solution

--- javascript-solutions.js
+++ javascript-solutions.js
@@ -265,3 +265,33 @@ function checkForFactor (base, factor) {
 }
 
 ________________________________________________
+
+//Vowel Count
+//Return the number(count) of vowels in the given string.
+//We will consider a,e,i,o,u as vowels for this Kata
+//The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str){
+  let count = 0;
+  for(let i = 0; i <= str.length; i++){
+    if(str[i] === "a"){
+      count +=1
+  }else if(str[i] === "e"){
+    count += 1
+  }else if(str[i] === "i"){
+    count += 1
+  }else if(str[i] === "o"){
+    count += 1
+  }else if (str[i] === "u"){
+    count += 1
+  }
+}
+return count;
+}
+
+
+function getCount(str) {
+  return str.split('').filter(c => "aeiouAEIOU".includes(c)).length;
+ }
+
+ _________________________________________________________________________________________--
