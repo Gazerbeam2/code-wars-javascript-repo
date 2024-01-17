1b55fc7a987b4b47f1bb3663ef1eb7b9610b912b
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2024-01-16 19:24:07 -0600

    added 6kyu-Persistent-Bugger

--- JS-Solutions/6kyu-Persistent-Bugger/6kyu-Persistent-Bugger.js
+++ JS-Solutions/6kyu-Persistent-Bugger/6kyu-Persistent-Bugger.js
@@ -10,3 +10,12 @@ function persistence(num){
 
     return times;
 }
+
+//Alternative Solutions
+
+function persistence(num){
+    for(var i = 0; num > 9; i++){
+        num = num.toString().split('').reduce((t,c) => c *t);
+    }
+    return i;
+}
