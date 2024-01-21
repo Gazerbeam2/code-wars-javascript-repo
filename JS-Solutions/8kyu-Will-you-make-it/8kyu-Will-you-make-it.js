e2c14bb4456905e198570e3326bf3d8786ac6d61
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2024-01-21 17:31:12 -0600

    added 8kyu-Will-you-make-it

---
+++ JS-Solutions/8kyu-Will-you-make-it/8kyu-Will-you-make-it.js
@@ -0,0 +1,9 @@
+const zeroFuel =(distanceToPump, mpg, fuelLeft) => {
+    return (mpg * fuelLeft) >= distanceToPump
+}
+
+//Distance to Pump is a set distance
+//We know how much the car runs on gallons
+//We know how many gallons that we have in our car
+//As long as the the amount of fuel that we have left is greater than the Distance, the car should make it.
+//So we multiply the amount of fuel that we have left over with the miles per hour and if the that product is greater or equal to the distance, return true, else return false
