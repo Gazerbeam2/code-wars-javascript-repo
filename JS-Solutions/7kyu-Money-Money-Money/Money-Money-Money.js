2cce10b1c295b0ae57c481f9ea3dcde39e1579a3
Gazerbeam2 <134249845+Gazerbeam2@users.noreply.github.com>
2024-01-27 19:57:36 -0600

    added Money,Money,Money comment

--- JS-Solutions/7kyu-Money-Money-Money/Money-Money-Money.js
+++ JS-Solutions/7kyu-Money-Money-Money/Money-Money-Money.js
@@ -4,7 +4,7 @@ function calculateYears(principal, interest, tax, desired) {
 
     let y = 0
     while(principal < desired){
-       principal += (principal * interest)  * (1 - tax)
+       principal += (principal * interest)  * (1 - tax) //When you pay taxes you dont get the 100% of your money, you get 100% - TAX(in percent, its the same as 1 - 0.18(in example)
       y++
     }
     return y
