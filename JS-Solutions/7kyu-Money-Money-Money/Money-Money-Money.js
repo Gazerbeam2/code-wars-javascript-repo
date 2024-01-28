ff3cf8ab25cc24e139bea1c6799147d22810ebd7
Gazerbeam2 <134249845+Gazerbeam2@users.noreply.github.com>
2024-01-27 19:15:54 -0600

    added Money,Money,Money

---
+++ JS-Solutions/7kyu-Money-Money-Money/Money-Money-Money.js
@@ -0,0 +1,12 @@
+//Solution
+
+function calculateYears(principal, interest, tax, desired) {
+
+    let y = 0
+    while(principal < desired){
+       principal += (principal * interest)  * (1 - tax)
+      y++
+    }
+    return y
+
+    }
