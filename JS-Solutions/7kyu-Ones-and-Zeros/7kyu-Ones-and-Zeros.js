bbeb3296e206d8ada32de39215d16ab5e8421256
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2024-02-07 19:34:50 -0600

    7kyu-Ones-and-Zeros

---
+++ JS-Solutions/7kyu-Ones-and-Zeros/7kyu-Ones-and-Zeros.js
@@ -0,0 +1,10 @@
+const binaryArrayToNumber = arr => {
+    let concatenatedNumbers = arr.join('');
+
+    return parseInt(concatenatedNumbers,2)
+};
+
+//We know that we start with a array
+//we know we need to return a integer
+//We have to concatenate the numbers together into a string
+//Concatenate the numbers and the return it as a decimal format
