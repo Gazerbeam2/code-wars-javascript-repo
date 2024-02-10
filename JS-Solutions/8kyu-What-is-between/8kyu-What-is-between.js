c8cb29d19bcf3d2d48551c480a64bc1d25805124
Gazerbeam2 <134249845+Gazerbeam2@users.noreply.github.com>
2024-02-09 22:08:34 -0600

    added 8kyu What is between

---
+++ JS-Solutions/8kyu-What-is-between/8kyu-What-is-between.js
@@ -0,0 +1,15 @@
+function between(a, b){
+    let newArray = []
+
+    for(let i = a; i <=b; i++){
+        newArray.push(i);
+    }return newArray
+}
+//Given two numbers
+//Are the numbers all positive? Are negative numbers included?
+//Are Strings a possibility
+//Create new Array
+//Start For Loop at first Parameter
+//Set the ending condition less than or equal to the second Parameter
+//Push the current index value to the new Array variable
+//Return the value of the new array
