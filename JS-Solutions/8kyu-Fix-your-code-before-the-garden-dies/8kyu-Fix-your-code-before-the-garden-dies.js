4d9b84f5ff45d1d39cf12ef0b061e587f85a0986
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2024-04-01 19:48:02 -0500

    8kyu-Fix-your-code-before-the-garden-dies

---
+++ JS-Solutions/8kyu-Fix-your-code-before-the-garden-dies/8kyu-Fix-your-code-before-the-garden-dies.js
@@ -0,0 +1,8 @@
+function rainAmount(mm){
+    if (mm < 40) {
+         return `You need to give your plant ${Math.abs(mm - 40)}mm of water`
+    }
+    else {
+         return "Your plant has had more than enough water for today!"
+    };
+}
