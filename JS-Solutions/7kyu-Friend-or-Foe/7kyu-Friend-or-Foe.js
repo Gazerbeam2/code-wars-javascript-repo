41b93a21068a865f4948bf2b106cdccb91d8d448
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2024-04-16 19:39:05 -0500

    7kyu-Friend-or-Foe?

---
+++ JS-Solutions/7kyu-Friend-or-Foe/7kyu-Friend-or-Foe.js
@@ -0,0 +1,11 @@
+function friend(friends){
+    return friends.filter((x) => x.length === 4)
+    //your code here
+  }
+
+  //create empty array
+  //loop through friends
+  //if current index of frieds has a length of 4, push the value to the new array
+  //return the new array
+
+  //
