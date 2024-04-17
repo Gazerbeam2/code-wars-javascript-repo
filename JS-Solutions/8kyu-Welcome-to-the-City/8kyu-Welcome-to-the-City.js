b00afa642bd8b926d3b319ab2b78cdeeec50db5e
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2024-04-16 19:39:50 -0500

    8kyu-Welcome-to-the-City

---
+++ JS-Solutions/8kyu-Welcome-to-the-City/8kyu-Welcome-to-the-City.js
@@ -0,0 +1,9 @@
+function sayHello( name, city, state ) {
+    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+  }
+
+
+
+  //The primary goal here is to take thise inputs and return them into a string
+  //Name will be in a array and will needed to be joined into a string
+  //join the name input and interpolate the rest of the inputs into a string.
