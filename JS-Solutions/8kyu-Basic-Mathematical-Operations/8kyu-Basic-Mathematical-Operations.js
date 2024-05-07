b1add467ca7202e7e0ae818d3c658625d2fbe8da
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2024-05-06 22:23:08 -0500

    added 8kyu-Basic-Mathematical-Operations

---
+++ JS-Solutions/8kyu-Basic-Mathematical-Operations/8kyu-Basic-Mathematical-Operations.js
@@ -0,0 +1,20 @@
+//Solution
+
+function basicOp(operation, value1, value2){
+    switch(operation){
+        case "+":
+        return value1 + value2
+        break;
+        case "-":
+        return value1 - value2;
+        break;
+        case "*" :
+       return value1 * value2;
+        break;
+        case "/" :
+       return value1 / value2;
+
+    }
+    //Code
+    return 0;
+  }
