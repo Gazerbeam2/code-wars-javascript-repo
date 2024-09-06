37f885f0c4352df544a992aea773bfa9a44659a0
Jordan Bryant Smith <jordanbryantsmith@Jordans-MacBook-Air.local>
2024-09-06 00:02:06 -0500

    added 8kyu-Enumerable-Magic-Does-My-List-Include-This

---
+++ JS-Solutions/8kyu-Enumerable-Magic-Does-My-List-Include-This/8kyu-Enumerable-Magic-Does-My-List-Include-This.js
@@ -0,0 +1,8 @@
+function include(arr, item){
+    return arr.includes(item);
+    // ...
+
+  }
+
+  //iterate through an array
+  //If current index of the array matches the desired value, return it
---
+++ JS-Solutions/8kyu-Enumerable-Magic-Does-My-List-Include-This/Readme.md
@@ -0,0 +1,3 @@
+8kyu-Enumerable Magic - Does My List Include This?
+
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
